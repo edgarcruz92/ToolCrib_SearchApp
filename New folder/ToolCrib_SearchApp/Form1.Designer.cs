b40274f89783b99cfcc6b458617ae8352a04f8e7
Edgar Cruz <edgarcruz92@icloud.com>
2022-07-09 09:49:37 -0600

    This is the first commit of the complete project

---
+++ New folder/ToolCrib_SearchApp/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace ToolCrib_SearchApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBoxDescription = new System.Windows.Forms.ComboBox();
+            this.textLocation = new System.Windows.Forms.TextBox();
+            this.textPrice = new System.Windows.Forms.TextBox();
+            this.textPartNumber = new System.Windows.Forms.TextBox();
+            this.textToSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textStock = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.textCurrency = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxDescription
+            // 
+            this.comboBoxDescription.FormattingEnabled = true;
+            this.comboBoxDescription.Location = new System.Drawing.Point(181, 292);
+            this.comboBoxDescription.Name = "comboBoxDescription";
+            this.comboBoxDescription.Size = new System.Drawing.Size(361, 23);
+            this.comboBoxDescription.TabIndex = 0;
+            this.comboBoxDescription.Text = "Description";
+            this.comboBoxDescription.SelectedIndexChanged += new System.EventHandler(this.comboBoxDescription_SelectedIndexChanged);
+            // 
+            // textLocation
+            // 
+            this.textLocation.Location = new System.Drawing.Point(548, 292);
+            this.textLocation.Name = "textLocation";
+            this.textLocation.Size = new System.Drawing.Size(100, 23);
+            this.textLocation.TabIndex = 1;
+            this.textLocation.Text = "Location";
+            // 
+            // textPrice
+            // 
+            this.textPrice.Location = new System.Drawing.Point(654, 292);
+            this.textPrice.Name = "textPrice";
+            this.textPrice.Size = new System.Drawing.Size(100, 23);
+            this.textPrice.TabIndex = 2;
+            this.textPrice.Text = "Price";
+            // 
+            // textPartNumber
+            // 
+            this.textPartNumber.Location = new System.Drawing.Point(54, 292);
+            this.textPartNumber.Name = "textPartNumber";
+            this.textPartNumber.Size = new System.Drawing.Size(121, 23);
+            this.textPartNumber.TabIndex = 3;
+            this.textPartNumber.Text = "Part Number";
+            // 
+            // textToSearch
+            // 
+            this.textToSearch.Location = new System.Drawing.Point(81, 177);
+            this.textToSearch.Name = "textToSearch";
+            this.textToSearch.Size = new System.Drawing.Size(677, 23);
+            this.textToSearch.TabIndex = 5;
+            this.textToSearch.Text = "What do you want to search?";
+            this.textToSearch.TextChanged += new System.EventHandler(this.textToSearch_TextChanged);
+            this.textToSearch.MouseEnter += new System.EventHandler(this.textToSearch_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LimeGreen;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(253, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(628, 57);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tool Crib Spare Parts Search Tool";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LimeGreen;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(298, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 37);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Search Components";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(773, 171);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(108, 32);
+            this.buttonSearch.TabIndex = 9;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textStock
+            // 
+            this.textStock.Location = new System.Drawing.Point(822, 292);
+            this.textStock.Name = "textStock";
+            this.textStock.Size = new System.Drawing.Size(85, 23);
+            this.textStock.TabIndex = 11;
+            this.textStock.Text = "Stock";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LimeGreen;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(73, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Part Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.LimeGreen;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(331, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Description";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.LimeGreen;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(566, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Location";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.LimeGreen;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(682, 269);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LimeGreen;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(846, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Stock";
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.Image")));
+            this.pictureBoxLogo.Location = new System.Drawing.Point(17, 11);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(204, 67);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLogo.TabIndex = 16;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // textCurrency
+            // 
+            this.textCurrency.Location = new System.Drawing.Point(760, 292);
+            this.textCurrency.Name = "textCurrency";
+            this.textCurrency.Size = new System.Drawing.Size(56, 23);
+            this.textCurrency.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.LimeGreen;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(756, 269);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Currency";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1011, 371);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textCurrency);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textStock);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textToSearch);
+            this.Controls.Add(this.textPartNumber);
+            this.Controls.Add(this.textPrice);
+            this.Controls.Add(this.textLocation);
+            this.Controls.Add(this.comboBoxDescription);
+            this.Name = "Form1";
+            this.Text = "   ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox comboBoxDescription;
+        private TextBox textLocation;
+        private TextBox textPrice;
+        private TextBox textPartNumber;
+        private TextBox textToSearch;
+        private Label label1;
+        private Label label3;
+        private Button buttonSearch;
+        private TextBox textStock;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label2;
+        private ToolTip toolTip1;
+        private PictureBox pictureBoxLogo;
+        private TextBox textCurrency;
+        private Label label8;
+    }
+}
